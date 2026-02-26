d05a58eeed77a4265eea2a86dd0780f9a2c767f8
ayush-h1 <146382093+ayush-h1@users.noreply.github.com>
2026-02-27 01:50:19 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,29 +1,16 @@
 const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   type: {
     type: String,
-    enum: ['investment', 'return', 'referral_bonus', 'withdrawal'],
+    enum: ['investment', 'return', 'referral_bonus', 'withdrawal', 'deposit', 'credit'], // added 'deposit' and 'credit'
     required: true
   },
-  amount: {
-    type: Number,
-    required: true
-  },
-  relatedId: {
-    type: mongoose.Schema.Types.ObjectId,
-    refPath: 'type' // dynamically reference Investment or Withdrawal
-  },
+  amount: { type: Number, required: true },
+  relatedId: { type: mongoose.Schema.Types.ObjectId, refPath: 'type' },
   description: String,
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
